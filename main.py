19426c8caebcb98472e8e0b5e0f1b13830910721
Smurf739 <dd.chelnik@gmail.com>
2023-11-27 15:08:06 +0300

    Git и желтые окружности

---
+++ main.py
@@ -0,0 +1,35 @@
+import random
+
+from UI import *
+from PyQt5 import QtWidgets
+import sys
+
+
+class Interface(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Interface, self).__init__()
+        self.check_db = None
+        self.base_line_edit = None
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.ellipse)
+        self.scene = QtWidgets.QGraphicsScene()
+        self.graphics_view = QtWidgets.QGraphicsView(self.ui.centralwidget)
+        self.graphics_view.setScene(self.scene)
+        self.graphics_view.setGeometry(290, 100, 200, 200)
+        self.graphics_view.setAlignment(Qt.AlignCenter)
+
+    def ellipse(self):
+        self.scene.clear()
+
+        diametr = random.randint(50, 187)
+        rect = QtCore.QRectF(0, 0, diametr, diametr)
+        color = Qt.yellow
+        self.scene.addEllipse(rect, QtGui.QPen(color), QtGui.QBrush(color))
+
+
+
+app = QtWidgets.QApplication(sys.argv)
+mywin = Interface()
+mywin.show()
+sys.exit(app.exec_())
